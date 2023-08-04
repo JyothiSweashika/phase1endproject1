a7045fb7d670aea0dea0dab350b11d7673a79d17
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-08-04 15:32:34 +0530

    pashe1endproject1

---
+++ ITeam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1endproject1
+{
+    internal class ITeam
+    {
+    }
+}
